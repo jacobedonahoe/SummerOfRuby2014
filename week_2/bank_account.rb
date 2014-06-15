11fe77bf04b4377952a38e88e0d7239eb93c35b0
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-06-14 20:44:18 -0500

    added transactions array and transaction hashes in withdrawl and deposit methods.

--- week_2/bank_account.rb
+++ week_2/bank_account.rb
@@ -1,5 +1,4 @@
 class BankAccount
-
   def to_s
     self.balance.to_s
   end
@@ -11,14 +10,19 @@ class BankAccount
   def deposit(amount)
     @balance = 0 if @balance.nil?
     @balance += amount
+    @transaction = {type: "deposit", amount: amount}
+    @transactions << @transaction
   end
 
   def init
     @balance = 0
+    @transactions = [:type,:amount]
   end
 
   def withdraw(amount)
     @balance -= amount
+    @transaction = {type: "withdrawl", amount: amount}
+    @transactions << @transaction
   end
 
   def name=(new_name)
@@ -37,3 +41,5 @@ account.deposit 20
 account.name= "TestAccount"
 
 puts %Q{The account "#{account.name}" has a balance of  $#{account.balance}.}
+
+
