817c5ff41b559d61649a709184e3c7da30fc4b63
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-06-23 15:16:49 -0500

    untested reconstruction of NAPCS's app.rb

--- week_2/cart/app.rb
+++ week_2/cart/app.rb
@@ -1,11 +1,48 @@
-class Cart
-  attr_accessor :name
-  attr_reader :total, :inventory
-  
-  def initialize
-    self.name = "test cart"
-  end
-end;
+require relative "lib/cart.rb"
+require relative "lib/item.rb"
+require relative "lib/money_helper.rb"
+
+include MoneyHelper
 
 cart = Cart.new
-puts cart.name
+
+items = [
+  {"a" => Item.new("apple",1.00)},
+  {"b" => Item.new("banana",1.00)},
+  {"c" => Item.new("carrot",0.50)},
+]
+
+keep_going = true
+
+def display_menu(items)
+  puts "Items Available:"
+  puts "-" * 10
+
+  items.each do |choice, item|
+    puts "#{choice} #{item.name} (#{format_currency(item.price)})"
+  end
+end
+
+while keep_going
+  puts "Choose an item"
+
+  display_menu(items)
+
+  choice = gets.chop
+
+  if items.has_key?(choice)
+    cart.add_to_cart(items[choice])
+  else
+    puts "That's not a valid option"
+  end
+
+  puts cart.summary
+
+  puts "More Items? (y or n)"
+
+  keep_going = gets.chop.downcase == "y"
+end
+
+puts cart.total
+
+puts "Thank you for shopping!"
