8e2e0b9161b8c2dcfaf788405722a253413e9c38
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-06-23 15:47:10 -0500

    specified items array as an instance variable with self. and @

--- week_2/cart/lib/cart.rb
+++ week_2/cart/lib/cart.rb
@@ -11,14 +11,14 @@ class Cart
     index = self.items.find_index{|entry| entry[:item]== item}
 
     if index
-      items[index][:quantity] += 1
+      self.items[index][:quantity] += 1
     else
-      items << {:item => item, :quantity => 1}
+      @items << {:item => item, :quantity => 1}
     end
   end
 
   def summary
-    self.items.collet{|item|
+    self.items.collect{|item|
       "Item : #{item[:item].name} @#{format_currency(item[item].price)} x #{item[:quantity]}"
     }.join("\n")
   end
