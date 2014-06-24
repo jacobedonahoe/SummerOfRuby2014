4d3a3ecc6a2938bd3f4f1d0a474162d4a83af5fa
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-06-23 19:20:23 -0500

    unworking uncertain why

--- week_3/environment.rb
+++ week_3/environment.rb
@@ -3,7 +3,7 @@ require 'rack'
 
 class HelloApp
   def call(env)
-    content = env.inspect
+    content = env.map {[key, value] "#{key} => #{value}"}.sort.join("\n")
     [200, {"Content-type" => "text/plain"}, [content]]
   end
 end
@@ -11,4 +11,4 @@ end
 app = HelloApp.new
 
 server = Rack::Server.new :app => app, :server=> "webrick" 
-server.star{
+server.start
