4ea26b5c50dedd811f5db8a7af2137f923f61d60
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-06-24 21:46:27 -0500

    non-functioning environment analysis

--- week_3/environment.rb
+++ week_3/environment.rb
@@ -1,9 +1,16 @@
-
 require 'rack'
 
+module BrowserDetection(browser_type)
+  if env.include?("Chrome")
+    content = "<h1>Hello there</h1>"
+  else
+    content = "I don't recognize your browser"
+  end
+end
+include BrowserDetection
 class HelloApp
   def call(env)
-    content = env.map {|key, value| "#{key} => #{value}"}.sort.join("\n")
+    self.browser_detection(env)
     [200, {"Content-type" => "text/plain"}, [content]]
   end
 end
