2d2c69a34ed2ce0cd647419ac61d3d0ae4c84cb0
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-07-07 12:53:04 -0500

    added products step

--- week_3/search/lib/browser.rb
+++ week_3/search/lib/browser.rb
@@ -1,1 +1,13 @@
-
+require_relative 'form.rb'
+class Browser
+  include Form
+  def call(env)
+    content = search_form
+    catalog = ProductCatalog.new
+    products = catalog.find_all
+    products.each  do |product|
+      content << "<p> #{product.name}: #{product.price}</p>"
+    end
+    [200, {"Content-type" => "text/html"}, [content]]
+  end
+end
