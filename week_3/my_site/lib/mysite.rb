0f6240fe0bada2177616c9071c97b8234cb80b07
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-07-11 17:09:09 -0500

    include RenderPage within class

--- week_3/my_site/lib/mysite.rb
+++ week_3/my_site/lib/mysite.rb
@@ -1,7 +1,8 @@
 require 'rack'
 require_relative 'render_page'
+require_relative 'handler'
 class Application
-  include RenderPage
+include RenderPage
   def self.run
     app = Rack::Builder.new do
       map "/" do
@@ -10,25 +11,25 @@ class Application
         self.render_page(page_name)
         }
       end
-      map "/about_me"
+      map "/about_me" do
         run lambda{|env|
         page_name ="about_me"
         self.render_page(page_name)
         }
       end
-      map "/resume"
+      map "/resume" do
         run lambda{|env|
         page_name="resume"
         self.render_page(page_name)
         }
       end
-      map "/contact"
+      map "/contact" do
         run lambda{|env|
         page_name="contact"
         self.render_page(page_name)
         }
       end
-      map "/submit_contact"
+      map "/submit_contact" do
         run Handler.new
       end
     end
