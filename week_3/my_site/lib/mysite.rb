fe98cb9f36655f30da06ed3bdd70f2d6000a0b4f
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-07-11 16:45:49 -0500

    added handler for post request

--- week_3/my_site/lib/mysite.rb
+++ week_3/my_site/lib/mysite.rb
@@ -1,23 +1,35 @@
 require 'rack'
 require_relative 'render_page'
-
 class Application
+  include RenderPage
   def self.run
     app = Rack::Builder.new do
       map "/" do
-        run HomePage.new
+        run lambda{|env|
+        page_name = "home_page"
+        self.render_page(page_name)
+        }
       end
       map "/about_me"
-        run AboutMePage.new
+        run lambda{|env|
+        page_name ="about_me"
+        self.render_page(page_name)
+        }
       end
       map "/resume"
-        run ResumePage.new
+        run lambda{|env|
+        page_name="resume"
+        self.render_page(page_name)
+        }
       end
       map "/contact"
-        run ContactPage.new
+        run lambda{|env|
+        page_name="contact"
+        self.render_page(page_name)
+        }
       end
       map "/submit_contact"
-        run SubmitContactPage.new
+        run Handler.new
       end
     end
   end
