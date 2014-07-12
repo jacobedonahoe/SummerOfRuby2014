e9c41127603faa434013c20f4329805f8b999c7f
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-07-12 01:04:17 -0500

    trying a better way

--- week_3/my_site/lib/mysite.rb
+++ week_3/my_site/lib/mysite.rb
@@ -1,38 +1,17 @@
 require 'rack'
 require_relative 'render_page'
 require_relative 'handler'
-include RenderPage
+
+class Page
+  include RenderPage
+  def call(env)
+    render_page(env,env["REQUEST_PATH"])
+  end
+end
+
 class Application
-  def self.run
-    app = Rack::Builder.new do
-      map "/" do
-        run lambda{|env|
-        page_name = "home_page"
-        self.render_page(page_name)
-        }
-      end
-      map "/about_me" do
-        run lambda{|env|
-        page_name ="about_me"
-        self.render_page(page_name)
-        }
-      end
-      map "/resume" do
-        run lambda{|env|
-        page_name="resume"
-        self.render_page(page_name)
-        }
-      end
-      map "/contact" do
-        run lambda{|env|
-        page_name="contact"
-        self.render_page(page_name)
-        }
-      end
-      map "/submit_contact" do
-        run Handler.new
-      end
-    end
+  def run
+    app = Page.new
   end
 end
 
