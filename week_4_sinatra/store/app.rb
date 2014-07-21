84bdf7067a070e3464a1bc5deefefeac48706122
Jacob Donahoe <jacob.e.donahoe@gmail.com>
2014-07-21 18:40:38 -0500

    modulized the app

--- week_4_sinatra/store/app.rb
+++ week_4_sinatra/store/app.rb
@@ -1,39 +1,43 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sqlite3'
 
-#assigning constant to refer to store database
-CONNECTION = SQLite3::Database.new("store.sqlite3")
+class MyApp < Sinatra::Base
 
-CONNECTION.execute <<-SQL
-  CREATE TABLE IF NOT EXISTS "items" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "name" VARCHAR,
-    "price" DOUBLE DEFAULT 0
-  )
-SQL
+  #assigning constant to refer to store database
+  CONNECTION = SQLite3::Database.new("store.sqlite3")
 
-get "/items" do
-  results = CONNECTION.execute("select name,price from items order by ROWID desc limit 20")
-  @tablerows= results.collect do |row|
-    "<tr><td>#{row[0]}</td><td>#{row[1]}</td></tr>"
+  CONNECTION.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS "items" (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "name" VARCHAR,
+      "price" DOUBLE DEFAULT 0
+    )
+  SQL
+
+  get "/items" do
+    results = CONNECTION.execute("select name,price from items order by ROWID desc limit 20")
+    @tablerows= results.collect do |row|
+      "<tr><td>#{row[0]}</td><td>#{row[1]}</td></tr>"
+    end
+    erb :items
   end
-  erb :items
-end
 
-get "/items/:id" do
-  id = params[:id]
-  result = CONNECTION.execute("select id,name,price from items where id=(?)", id)
-  item = result[0]
-  @output = "<tr><td>#{item[0]}</td><td>#{item[1]}</td><td>#{item[2]}</td></tr>"
-  erb :id
-end
+  get "/items/:id" do
+    id = params[:id]
+    result = CONNECTION.execute("select id,name,price from items where id=(?)", id)
+    item = result[0]
+    @output = "<tr><td>#{item[0]}</td><td>#{item[1]}</td><td>#{item[2]}</td></tr>"
+    erb :id
+  end
 
-post "/items" do
-  name = params[:name]
-  price = params[:price]
+  post "/items" do
+    name = params[:name]
+    price = params[:price]
 
-  CONNECTION.execute("INSERT INTO items
-                     (name, price) VALUES (?,?)", [name, price])
+    CONNECTION.execute("INSERT INTO items
+                      (name, price) VALUES (?,?)", [name, price])
 
-  redirect "/items"
+    redirect "/items"
+  end
+
 end
